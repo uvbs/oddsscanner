196e1a1e390d00f2b19cdfca2b958d6e6a52e96e
behappyall <not.happy.be.happy@gmail.com>
2017-07-02 21:10:59 +0300

    go test.

--- oddsscanner/Program.cs
+++ oddsscanner/Program.cs
@@ -11,6 +11,7 @@ using System.Text;
 using HtmlAgilityPack;
 using OddsAnalyzer;
 using BookmakerParser;
+using BetsLibrary;
 
 namespace oddsscanner
 {
@@ -24,27 +25,63 @@ namespace oddsscanner
             //Marathonbet marathon = new Marathonbet();
             LeonBets leon = new LeonBets();
 
-            //finder.AddBookmaker(marathon);
-           finder.AddBookmaker(leon);
+           // finder.AddBookmaker(marathon);
+            finder.AddBookmaker(leon);
 
             
             var result = finder.GetArbitrageBets();
 
             Console.WriteLine("here");
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 20; i++)
             {
-                  result =   finder.GetArbitrageBets();
-             //   marathon.Parse();
+                  result = finder.GetArbitrageBets();
+                //   marathon.Parse();
                 Thread.Sleep(2000);
             }
 
-            List<BetsLibrary.Bet> list = new List<BetsLibrary.Bet>();
+                List<BetsLibrary.Bet> list = new List<BetsLibrary.Bet>();
 
             foreach(var bet in leon.GetBetList())
             {
                 if (!list.Contains(bet)) list.Add(bet);
-                else throw new Exception();
+                else
+                {
+                    Console.WriteLine("error");
+                    Console.WriteLine();
+                    Console.Write("{0} vs {1}   ", bet.MatchName.FirstTeam, bet.MatchName.SecondTeam);
+                    Console.Write("Sport : {0}  ", bet.Sport);
+                    try
+                    {
+                        var T = bet as ResultBet;
+                        if (T != null)
+                        {
+                            Console.Write("{0}", ((ResultBet)bet).ToString());
+                            Console.Write("coef: {0}", T.Odds);
+                        }
+                    }
+                    catch (Exception ex) { Console.WriteLine(ex.Message); }
+                    try
+                    {
+                        var T = bet as TotalBet;
+                        if (T != null)
+                        {
+                            Console.Write("{0}", ((TotalBet)bet).ToString());
+                            Console.Write("coef: {0}", T.Odds);
+                        }
+                    }
+                    catch { }
+                    try
+                    {
+                        var h = bet as HandicapBet;
+                        if (h != null)
+                        {
+                            Console.Write("{0}", ((HandicapBet)bet).ToString());
+                            Console.Write("coef: {0}", h.Odds);
+                        }
+                    }
+                    catch { }
+                }
             }
             
 
